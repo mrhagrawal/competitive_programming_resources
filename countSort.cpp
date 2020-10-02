bff1197db46c99edd3a6847e66a3cfd715c48f9c
prof-lupin <33233393+prof-lupin@users.noreply.github.com>
2020-10-02 09:42:31 +0530

    Comments to countSort.cpp were added

--- countSort.cpp
+++ countSort.cpp
@@ -1,14 +1,21 @@
+// prof-lupin
+// Arya Shrivastava
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
   int init[100], rec[1000]={0}, j=0, n, i;
-  cin >> n;
+  cin >> n; // enter the length of the array
+  
+  // taking input into the array
   for(i=0; i<n; i++)
   {
     cin >> init[i];
     rec[init[i]]++;
   }
+  
+  // lines: 20 to 30; count sort logic
   i=0;
   while(j<(*(&rec+1)-rec) && i<n)
   {
@@ -21,10 +28,13 @@ int main(int argc, char const *argv[]) {
     else
       j++;
   }
+  
+  // displaying the sorted array
   for(i=0; i<n; i++)
   {
     cout << init[i] <<" ";
   }
   cout << endl;
+  
   return 0;
 }
